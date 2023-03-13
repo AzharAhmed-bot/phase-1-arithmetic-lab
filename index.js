e5cfe5e68598f481edfb3aa06ff46b91f9a01b05
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-03-13 23:11:15 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let multiply;
+let num1=31;
+let num2=2;
+multiply=num1*num2;
 
-// Write your code here
+console.log(multiply);
+var random= (Math.floor(Math.random(random))*10)+1
+console.log(random);
+var mod;
+var num3=10
+var num4=6
+mod=num3 % num4;
+console.log(mod)
+var max=Math.max(1,2,3,4,5,10,20);
+console.log(max);
+
